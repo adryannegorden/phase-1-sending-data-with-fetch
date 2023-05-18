6dd933a84191039c89659a079cd0d4c272e550a6
adryannegorden <adryannegorden@gmail.com>
2023-05-18 15:53:00 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
-// Add your code here
+function submitData(name, email) {
+    const url = "http://localhost:3000/users";
+    const headers = {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    };
+    const body = JSON.stringify({ name: name, email: email });
+  
+    return fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: body
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+  }
+
+  function submitData(name, email) {
+    const url = "http://localhost:3000/users";
+    const headers = {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    };
+    const body = JSON.stringify({ name: name, email: email });
+  
+    return fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: body
+    })
+    .then(response => response.json())
+    .then(data => {
+      const id = data.id; // Access the newly assigned ID from the response object
+      const idElement = document.createElement("p");
+      idElement.textContent = id;
+      document.body.appendChild(idElement);
+    })
+    .catch(error => console.log(error));
+  }
+
+  function submitData(name, email) {
+    const url = "http://localhost:3000/users";
+    const headers = {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    };
+    const body = JSON.stringify({ name: name, email: email });
+  
+    return fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: body
+    })
+    .then(response => response.json())
+    .then(data => {
+      const id = data.id;
+      const idElement = document.createElement("p");
+      idElement.textContent = id;
+      document.body.appendChild(idElement);
+    })
+    .catch(error => {
+      const errorMessage = error.message; // Access the error message
+      const errorElement = document.createElement("p");
+      errorElement.textContent = errorMessage;
+      document.body.appendChild(errorElement);
+    });
+  }
+  
